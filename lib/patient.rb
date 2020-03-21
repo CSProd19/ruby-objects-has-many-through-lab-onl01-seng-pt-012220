9e5acb2ded2ca212100c7357ffc7218967b1afc2
Maria Connie Sanchez <msnchz04@gmail.com>
2020-03-21 22:55:23 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,26 @@
+class Patient 
+  attr_accessor :name, :appointment, :doctor
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def new_appointment(doctor, date)
+    Appointment.new(doctor, date, self)
+  end
+  
+  def appointments 
+    Appointment.all.select {|a| a.patient == self }
+  end
+  
+  def doctors 
+    self.appointments.collect {|a| a.doctor}
+  end 
+end 
